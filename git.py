710f6e46457d57e8084d7dc8ac29639372a6349c
manohar yadav <k.manoharyadav26@gmail.com>
2024-06-26 18:41:14 +0530

    addded url in git.py

--- git.py
+++ git.py
@@ -4,7 +4,7 @@ def git_clone(repo_url, destination_path):
     git.Repo.clone_from(repo_url, destination_path)
  
 # Example usage:
-repo_url = "https://github.com/example_user/example_repo.git"
+repo_url = "https://github.com/manohar890/pythontest"
 destination_path = "./folder"
  
 git_clone(repo_url, destination_path)
